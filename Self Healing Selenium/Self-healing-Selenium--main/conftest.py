f40866a3ecee0bffebd6810f1478281f68e70d0f
varshaps27 <psvarsha27082005@gmail.com>
2026-06-10 01:12:17 +0530

    Delete Self Healing Selenium/Self-healing-Selenium--main directory

--- Self Healing Selenium/Self-healing-Selenium--main/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from core.driver_factory import DriverFactory
-
-
-@pytest.fixture
-def driver():
-    driver = DriverFactory.get_driver()
-    yield driver
-    driver.quit()
--- Self Healing Selenium/Self-healing-Selenium--main/self -healing- frameworks/self-healing-framework/core/config.py
+++
@@ -1,9 +0,0 @@
-# Central config for the framework
-
-BASE_URL = "https://opensource-demo.orangehrmlive.com/"
-
-LOCATOR_FILE = "locators/login_locators.json"
-
-IMPLICIT_WAIT = 10
-
-BROWSER = "chrome"
